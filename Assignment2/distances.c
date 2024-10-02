2d3f2beeb52b7c8fd74e53bda46f1993093bfc3b
Simon Berggren <simon.berggren@hotmail.com>
2024-10-02 08:54:23 +0200

    We gt a step mark at 860 mikroseconds

--- Assignment2/distances.c
+++ Assignment2/distances.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-// #include <time.h>
+#include <time.h>
 
 // anvand float istallet for double, osb! eg sqrtf() istallet for sqrt() 
 // kolla pa locality_block_auxillary.c
@@ -30,7 +30,7 @@ int main(int argc, char const *argv[])
     9    00111001    071     39
     */
     
-    const int rows_per_block = 10; 
+    const int rows_per_block = 10000; 
     const int char_per_row = 24;
     const int max_distance = 3465; // = sqrt(20^2*3)
     
@@ -42,7 +42,7 @@ int main(int argc, char const *argv[])
     }
 
 
-    FILE *file = fopen("cells", "r");
+    FILE *file = fopen("cells_1e4", "r");
 
     if (file == NULL) {
         printf("Error opening file cells!\n");
@@ -77,11 +77,19 @@ int main(int argc, char const *argv[])
     // Calculate the distances (inte kollat på vilka distanser som räknas...)
     for (int ix = 0; ix < rows_per_block; ++ix){
         for (int jx = ix + 1; jx < rows_per_block; ++jx){
-            float dist = distance(rows[ix], rows[jx]);
-            int dist_int = (int) (dist * 100);
+            float dist = distance(rows[ix], rows[jx]) * 100;
+            dist = round(dist);
+            int dist_int = (int) (dist);
             result[dist_int] += 1;
         }
     }
+
+    // Initialize the random number generator
+    srand(time(NULL));
+    // Generate a random index between 0 and rows_per_block
+    int ix = rand() % (rows_per_block + 1);
+    // Print the value at rows[ix][2]
+    printf("Random value from rows[%i][2]: %f\n", ix, rows[ix][2]);
 
     // RESULT
     for (int ix = 0; ix < 3465; ++ix){
@@ -89,6 +97,9 @@ int main(int argc, char const *argv[])
             printf("%02d.%02d %d\n", ix/100, ix%100, result[ix]);
         }
     }
+
+    // This seems to work/print the correct stuff
+    // printf("%f\n", rows[6][2]);
 
     free(result);
     free(cells);
